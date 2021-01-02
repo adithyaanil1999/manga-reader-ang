cfddeb6dcbf3bbeeef37c954f95e6dbe2bfad4d9
adithyaanil <adithyas.anil99@gmail.com>
2021-01-02 23:57:15 +0400

    added error handling

--- src/global.ts
+++ src/global.ts
@@ -8,7 +8,7 @@ let currentUrl = window.location.href;
 
 export const prodBool = currentUrl.indexOf('github.io') !== -1 ? false : true;
 
-export const version = '1.1.3';
+export const version = '1.1.4';
 
 export const defaultSRC = 'MGFX';
 
